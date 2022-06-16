8a7bf8b444cf010179f489853af87f6427a49d12
Miguel Gargallo <mgargallo16@gmail.com>
2022-06-16 12:48:35 +0200

    1.1.1 Pruebas 1.1.1 Pruebas

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import { useState } from "react";
 const rootElement = document.getElementById("root");
 
 const App = (props) => {
+  let contador = 0;
+
+  setInterval(() => {
+    contador++;
+  }, 1000);
+
   return (
     <div>
-      <h1>{props.contadorInicial}</h1>
+      <h1>{contador}</h1>
     </div>
   );
 };
